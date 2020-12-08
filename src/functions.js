a908a7534e5ad0abad70ce1fc68c8c76892dc84b
MatheusLetra <jrtyuw@gmail.com>
2020-12-08 14:30:30 -0300

    alterções 08-12

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,5 @@
 const createBoard = (rows, columns) => {
-    return Array(row).fill(0).map((_, row) => {
+    return Array(rows).fill(0).map((_, row) => {
         return Array(columns).fill(0).map((_,column)=> {
             return {
                 row,
@@ -34,6 +34,41 @@ const createMinedBoard = (rows,columns, minesAmount) => {
     const board = createBoard(rows, columns)
     spreadMines(board,minesAmount)
     return board
+}
+
+const cloneBoard = board => {
+    return board.map(rows => {
+        return rows.map(field => {
+            return { ...field }
+        })
+    })
+}
+
+const getNeighbors = (board, row, column) => {
+    const neighbors = []  
+    const rows = [row - 1, row, row + 1]
+    const columns = [column -1, column, column + 1]
+    rows.forEach(r => {
+        columns.forEach(c => {
+            const diferent = r !== row || c !== column
+            const validRow = r >= 0 && r < board.length
+            const validColumn = c >= 0 && c < board[0].length
+
+            if (diferent && validColumn && validRow) {
+                neighbors.push(board[r][c])
+            }
+        })
+    })
+    return neighbors
+}
+
+const safeNeighborhood = (board, row, column) => {
+    const safes = (result, neighbor) => result && !neighbor.mined
+    return getNeighbors(board, row, column).reduce(safes,true)
+}
+
+const openField  = (board, row, column) => {
+
 }
 
 export { createMinedBoard }
